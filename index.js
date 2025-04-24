c24387bacd2beb84d3fbd80c72dfffd64de4b7e4
shaunieboy31 <157598882+shaunieboy31@users.noreply.github.com>
2025-04-24 14:44:28 +0800

    TRY 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app'; // Corrected the path to match the file structure
+<script type="text/babel">
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App from './app'; // Ensure the path is correct
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<App />, document.getElementById('root'));
+</script>
